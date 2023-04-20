c4c2ca8035838f525896ece0550355c363131fa2
lpite <farionsasa@gmail.com>
2023-04-20 20:23:33 +0300

    kinda noram code :)

--- lab3/MyForm.h
+++ lab3/MyForm.h
@@ -2,7 +2,15 @@
 #include <cstring>
 #include <math.h>
 
-
+struct VECTOR {
+	double x;
+	double y;
+	double	length;
+ };
+struct POINT {
+	double x;
+	double y;
+};
 namespace lab3 {
 
 	using namespace System;
@@ -65,9 +73,9 @@ namespace lab3 {
 	private: System::Windows::Forms::TabPage^ tabPage1;
 	private: System::Windows::Forms::TabPage^ tabPage2;
 	private: System::Windows::Forms::Label^ Result;
-	private: System::Windows::Forms::Label^ label2;
-	private: System::Windows::Forms::Label^ label3;
-	private: System::Windows::Forms::Label^ label4;
+
+
+
 	private: System::Windows::Forms::TextBox^ A1_Input;
 	private: System::Windows::Forms::TextBox^ A2_Input;
 	private: System::Windows::Forms::TextBox^ C2_Input;
@@ -118,9 +126,6 @@ namespace lab3 {
 			this->label6 = (gcnew System::Windows::Forms::Label());
 			this->label7 = (gcnew System::Windows::Forms::Label());
 			this->A1_Input = (gcnew System::Windows::Forms::TextBox());
-			this->label2 = (gcnew System::Windows::Forms::Label());
-			this->label3 = (gcnew System::Windows::Forms::Label());
-			this->label4 = (gcnew System::Windows::Forms::Label());
 			this->Result = (gcnew System::Windows::Forms::Label());
 			this->tabPage2 = (gcnew System::Windows::Forms::TabPage());
 			this->contextMenuStrip2 = (gcnew System::Windows::Forms::ContextMenuStrip(this->components));
@@ -139,21 +144,21 @@ namespace lab3 {
 			this->B1_Input->Location = System::Drawing::Point(6, 66);
 			this->B1_Input->Name = L"B1_Input";
 			this->B1_Input->Size = System::Drawing::Size(38, 20);
-			this->B1_Input->TabIndex = 2;
+			this->B1_Input->TabIndex = 3;
 			// 
 			// C1_Input
 			// 
 			this->C1_Input->Location = System::Drawing::Point(6, 92);
 			this->C1_Input->Name = L"C1_Input";
 			this->C1_Input->Size = System::Drawing::Size(38, 20);
-			this->C1_Input->TabIndex = 3;
+			this->C1_Input->TabIndex = 5;
 			// 
 			// CountButton
 			// 
 			this->CountButton->Location = System::Drawing::Point(6, 141);
 			this->CountButton->Name = L"CountButton";
 			this->CountButton->Size = System::Drawing::Size(75, 23);
-			this->CountButton->TabIndex = 4;
+			this->CountButton->TabIndex = 7;
 			this->CountButton->Text = L"Count";
 			this->CountButton->UseVisualStyleBackColor = true;
 			this->CountButton->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
@@ -165,7 +170,7 @@ namespace lab3 {
 			this->Cos->Location = System::Drawing::Point(4, 3);
 			this->Cos->Name = L"Cos";
 			this->Cos->Size = System::Drawing::Size(88, 17);
-			this->Cos->TabIndex = 5;
+			this->Cos->TabIndex = 8;
 			this->Cos->TabStop = true;
 			this->Cos->Text = L"Косинус B/2";
 			this->Cos->UseVisualStyleBackColor = true;
@@ -177,7 +182,7 @@ namespace lab3 {
 			this->Height->Location = System::Drawing::Point(3, 26);
 			this->Height->Name = L"Height";
 			this->Height->Size = System::Drawing::Size(74, 17);
-			this->Height->TabIndex = 6;
+			this->Height->TabIndex = 9;
 			this->Height->Text = L"висоту ha";
 			this->Height->UseVisualStyleBackColor = true;
 			this->Height->CheckedChanged += gcnew System::EventHandler(this, &MyForm::Height_CheckedChanged);
@@ -196,27 +201,27 @@ namespace lab3 {
 			this->A_Label->AutoSize = true;
 			this->A_Label->Location = System::Drawing::Point(50, 43);
 			this->A_Label->Name = L"A_Label";
-			this->A_Label->Size = System::Drawing::Size(20, 13);
+			this->A_Label->Size = System::Drawing::Size(19, 13);
 			this->A_Label->TabIndex = 8;
-			this->A_Label->Text = L"A1";
+			this->A_Label->Text = L"Ax";
 			// 
 			// B_Label
 			// 
 			this->B_Label->AutoSize = true;
 			this->B_Label->Location = System::Drawing::Point(50, 69);
 			this->B_Label->Name = L"B_Label";
-			this->B_Label->Size = System::Drawing::Size(20, 13);
+			this->B_Label->Size = System::Drawing::Size(19, 13);
 			this->B_Label->TabIndex = 9;
-			this->B_Label->Text = L"B1";
+			this->B_Label->Text = L"Bx";
 			// 
 			// C_Label
 			// 
 			this->C_Label->AutoSize = true;
 			this->C_Label->Location = System::Drawing::Point(50, 95);
 			this->C_Label->Name = L"C_Label";
-			this->C_Label->Size = System::Drawing::Size(20, 13);
+			this->C_Label->Size = System::Drawing::Size(19, 13);
 			this->C_Label->TabIndex = 10;
-			this->C_Label->Text = L"C1";
+			this->C_Label->Text = L"Cx";
 			// 
 			// label1
 			// 
@@ -246,9 +251,6 @@ namespace lab3 {
 			this->tabPage1->Controls->Add(this->label6);
 			this->tabPage1->Controls->Add(this->label7);
 			this->tabPage1->Controls->Add(this->A1_Input);
-			this->tabPage1->Controls->Add(this->label2);
-			this->tabPage1->Controls->Add(this->label3);
-			this->tabPage1->Controls->Add(this->label4);
 			this->tabPage1->Controls->Add(this->Result);
 			this->tabPage1->Controls->Add(this->C1_Input);
 			this->tabPage1->Controls->Add(this->label1);
@@ -271,82 +273,55 @@ namespace lab3 {
 			this->A2_Input->Location = System::Drawing::Point(76, 40);
 			this->A2_Input->Name = L"A2_Input";
 			this->A2_Input->Size = System::Drawing::Size(38, 20);
-			this->A2_Input->TabIndex = 22;
+			this->A2_Input->TabIndex = 2;
 			// 
 			// C2_Input
 			// 
 			this->C2_Input->Location = System::Drawing::Point(76, 92);
 			this->C2_Input->Name = L"C2_Input";
 			this->C2_Input->Size = System::Drawing::Size(38, 20);
-			this->C2_Input->TabIndex = 18;
+			this->C2_Input->TabIndex = 6;
 			// 
 			// B2_Input
 			// 
 			this->B2_Input->Location = System::Drawing::Point(76, 66);
 			this->B2_Input->Name = L"B2_Input";
 			this->B2_Input->Size = System::Drawing::Size(38, 20);
-			this->B2_Input->TabIndex = 17;
+			this->B2_Input->TabIndex = 4;
 			// 
 			// label5
 			// 
 			this->label5->AutoSize = true;
 			this->label5->Location = System::Drawing::Point(120, 95);
 			this->label5->Name = L"label5";
-			this->label5->Size = System::Drawing::Size(20, 13);
+			this->label5->Size = System::Drawing::Size(19, 13);
 			this->label5->TabIndex = 21;
-			this->label5->Text = L"C2";
+			this->label5->Text = L"Cy";
 			// 
 			// label6
 			// 
 			this->label6->AutoSize = true;
 			this->label6->Location = System::Drawing::Point(120, 69);
 			this->label6->Name = L"label6";
-			this->label6->Size = System::Drawing::Size(20, 13);
+			this->label6->Size = System::Drawing::Size(19, 13);
 			this->label6->TabIndex = 20;
-			this->label6->Text = L"B2";
+			this->label6->Text = L"By";
 			// 
 			// label7
 			// 
 			this->label7->AutoSize = true;
 			this->label7->Location = System::Drawing::Point(120, 43);
 			this->label7->Name = L"label7";
-			this->label7->Size = System::Drawing::Size(20, 13);
+			this->label7->Size = System::Drawing::Size(19, 13);
 			this->label7->TabIndex = 19;
-			this->label7->Text = L"A2";
+			this->label7->Text = L"Ay";
 			// 
 			// A1_Input
 			// 
 			this->A1_Input->Location = System::Drawing::Point(6, 40);
 			this->A1_Input->Name = L"A1_Input";
 			this->A1_Input->Size = System::Drawing::Size(38, 20);
-			this->A1_Input->TabIndex = 16;
-			// 
-			// label2
-			// 
-			this->label2->AutoSize = true;
-			this->label2->Location = System::Drawing::Point(233, 150);
-			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(14, 13);
-			this->label2->TabIndex = 15;
-			this->label2->Text = L"C";
-			// 
-			// label3
-			// 
-			this->label3->AutoSize = true;
-			this->label3->Location = System::Drawing::Point(233, 124);
-			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(14, 13);
-			this->label3->TabIndex = 14;
-			this->label3->Text = L"B";
-			// 
-			// label4
-			// 
-			this->label4->AutoSize = true;
-			this->label4->Location = System::Drawing::Point(233, 98);
-			this->label4->Name = L"label4";
-			this->label4->Size = System::Drawing::Size(14, 13);
-			this->label4->TabIndex = 13;
-			this->label4->Text = L"A";
+			this->A1_Input->TabIndex = 1;
 			// 
 			// Result
 			// 
@@ -390,27 +365,36 @@ namespace lab3 {
 		}
 #pragma endregion
 	int whatChecked = 0;
-	int A1 = 0, A2 = 0, B1= 0, B2=0,C1=0,C2=0;
+
 	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
 		try
 		{
-			double a1 = Convert::ToDouble(A1_Input->Text);
-			double a2 = Convert::ToDouble(A2_Input->Text);
-			double b1 = Convert::ToDouble(B1_Input->Text);
-			double b2 = Convert::ToDouble(B2_Input->Text);
-			double c1 = Convert::ToDouble(C1_Input->Text);
-			double c2 = Convert::ToDouble(C2_Input->Text);
-			
-			double a_b = sqrt(pow(b1 - a1,2) + pow(b2 - a2,2));
-			double b_c = sqrt(pow(c1 - b1, 2) + pow(c2 - b2, 2));
-			double c_a = sqrt(pow(a1 - c1, 2) + pow(a2 - c2, 2));
-			double skalar1 = a1 * b1 + a2 * b2;
-			double skalar2 = b1 * c1 + b2 * c2;
-			double module1 = sqrt(pow(a1,2)+pow(a2,2));
-			double module2 = sqrt(pow(b1, 2) + pow(b2, 2));
+			POINT A{}, B{}, C{};
+			A.x = Convert::ToDouble(A1_Input->Text);
+			A.y = Convert::ToDouble(A2_Input->Text);
 
-			double idk =skalar1*skalar2/fabs(sqrt(a1*a1+a2*a2))/ fabs(sqrt(b1 * b1 + b2 * b2));
-			Result->Text = Convert::ToString(idk);
+			B.x = Convert::ToDouble(B1_Input->Text);
+			B.y = Convert::ToDouble(B2_Input->Text);
+
+			C.x = Convert::ToDouble(C1_Input->Text);
+			C.y = Convert::ToDouble(C2_Input->Text);
+
+			VECTOR AB{
+				B.x - A.x,
+				B.y - A.y,
+				sqrt(pow(AB.x,2)+pow(AB.y,2))
+			}, 
+			BC{
+				C.x - B.x,
+				C.y - B.y,
+				sqrt(pow(BC.x,2) + pow(BC.y,2))
+			}, 
+			CA{
+				A.x - C.x,
+				A.y - C.y,
+				sqrt(pow(CA.x,2) + pow(CA.y,2))
+			};
+			Result->Text = Convert::ToString(AB.length);
 			if (whatChecked)
 			{
 				
